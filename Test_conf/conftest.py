db63e5ac4efe95aa11afa738684f79069114adc7
Boaz <boazusa5@hotmail.com>
2024-12-29 16:15:53 +0200

    12_29_2024 update #1

--- Test_conf/conftest.py
+++ Test_conf/conftest.py
@@ -1,8 +1,8 @@
 # conftest.py
 def pytest_addoption(parser):
     parser.addoption(
-        "--KEY", action="store", default="default_value1", help="First input parameter"
+        "--KEY", action="store", default="default_value1", help="First input API key parameter"
     )
     parser.addoption(
-        "--KEY2", action="store", default="default_value2", help="Second input parameter"
+        "--KEY2", action="store", default="default_value2", help="Second input API key parameter"
     )
